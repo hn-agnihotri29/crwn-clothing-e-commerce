e81c9b4b833547975fa09c96622e64949e9bd3e6
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2023-07-27 13:31:16 +0530

    Routing steup with home and navigation

---
+++ src/routes/navigation/navigation.component.jsx
@@ -0,0 +1,28 @@
+//Fragement don't display the wrap div in our real dom
+import { Fragment } from "react"
+import { ReactComponent as CrwnLogo } from "../../assets/crown.svg"
+//Link is act as achor tag
+import { Outlet, Link } from "react-router-dom"
+
+import './navigation.styles.scss'
+
+
+const Navigation = () => {
+    return (
+      <Fragment>
+        <div className="navigation">
+            <Link className="logo-container" to='/'>
+                <CrwnLogo className="logo" />
+            </Link>
+            <div className="nav-links-container"> 
+                <Link className="nav-link" to='/shop'>
+                    SHOP
+                </Link>
+            </div>
+        </div>
+        <Outlet />
+      </Fragment>
+    )
+}
+
+export default Navigation
