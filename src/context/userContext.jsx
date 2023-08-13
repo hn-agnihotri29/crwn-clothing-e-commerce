cfd82bfcdd4349678a6b5fe6e242fd31ea8d6764
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2023-08-13 14:35:02 +0530

    modify userContext and CartContext using useReducer

--- src/context/userContext.jsx
+++ src/context/userContext.jsx
@@ -1,4 +1,4 @@
-import { createContext, useState,useEffect } from "react";
+import { createContext,useEffect, useReducer } from "react";
 
 import { onAuthStateChangedListener,createUserDocFromAuth } from "../utils/firebase/firebase.utils";
 
@@ -13,8 +13,42 @@ export const UserContext = createContext({
 //value hold actual context value
 //this provider allowing any of child component to access the value inside the useState
 
+
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: 'SET_CURRENT_USER'
+}
+
+const useerReducer = (state, action) => {
+  // payload store the value that is important for reducer to know what to update
+  const {type, payload } = action
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        ...state,
+        currentUser: payload
+      }
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`)
+  }
+}
+
+const INITIAL_STATE = {
+  currentUser: null
+}
+
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+
+  //useReducer takes reducer function and initial value 
+  const [{currentUser}, dispatch] = useReducer(useerReducer, INITIAL_STATE)
+  
+
+  const setCurrentUser = (user) => {
+    dispatch({type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user})
+  }
+
+
+  //const [currentUser, setCurrentUser] = useState(null);
   const value = { currentUser, setCurrentUser };
 
 
