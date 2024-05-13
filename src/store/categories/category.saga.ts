1a8b2975b503897ee7bcc5d9d424fc8232a66cb6
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-05-13 22:54:10 +0530

    migrating the category saga into the typescript.

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { all, call, put, takeLatest } from "redux-saga/effects"
+import { all, call, put, takeLatest } from "typed-redux-saga"
 
 import { getCollectionAndDocument } from "../../utils/firebase/firebase.utils"
 
@@ -11,16 +11,16 @@ import { CATEGORIES_ACTION_TYPES } from './category.types';
   
 export function* fetchCategoriesAsync() {
 try {
-    const categoriesArray = yield call(getCollectionAndDocument, 'categories');
-    yield put(fetchCategoriesSuccess(categoriesArray));
+    const categoriesArray = yield* call(getCollectionAndDocument);
+    yield* put(fetchCategoriesSuccess(categoriesArray));
 } catch (error) {
-    yield put(fetchCategoriesFailed(error));
+    yield* put(fetchCategoriesFailed(error as Error));
 }
 }
-  
+   
 export function* onFetchCategories() {
 //takeLatest--> take receive the actions, if we have bunch of same action they give latest one 
-yield takeLatest(
+yield* takeLatest(
     CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START,
     fetchCategoriesAsync
 );
@@ -28,5 +28,5 @@ yield takeLatest(
   
 export function* categoriesSaga() {
     //all---> run everything inside and only complete if all of it is done
-    yield all([call(onFetchCategories)]);
+    yield* all([call(onFetchCategories)]);
 }
