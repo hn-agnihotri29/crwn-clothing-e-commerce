c64ad0d64f25088a197b57486656e91656d787ca
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-05-13 14:53:05 +0530

    migrating the js to typescript uses the withMatcher function to narrowing the function for dispatched action.

--- src/store/store.js
+++ src/store/store.js
@@ -2,6 +2,7 @@ import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
+
 import { persistStore, persistReducer } from "redux-persist";
 import storage from 'redux-persist/lib/storage' // defaults to localStorage for web
 //import thunk from "redux-thunk";
