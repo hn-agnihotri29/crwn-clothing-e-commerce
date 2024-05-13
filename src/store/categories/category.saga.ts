034e3c55d7eb43b6b57a97067c925825117cb1c5
Harshaja Agnihotri <hn.agnihotri29@gmail.com>
2024-05-13 23:25:17 +0530

    migrating the user saga to typescript

--- src/store/categories/category.saga.ts
+++ src/store/categories/category.saga.ts
@@ -1,4 +1,4 @@
-import { all, call, put, takeLatest } from "typed-redux-saga"
+import { all, call, put, takeLatest } from "typed-redux-saga/macro"
 
 import { getCollectionAndDocument } from "../../utils/firebase/firebase.utils"
 
